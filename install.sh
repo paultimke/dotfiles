b8fd86f66456f0759451661f96dd6c74a9affdb3
Paul Timke <87957114+paultimke@users.noreply.github.com>
2022-12-29 13:36:58 -0600

    Added installation script for symlinks

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the name of the current working directory (dotfiles folder)
+cwd=${PWD##*/}
+cwd=${cwd:-/}
+
+# Create an array with all the dotfiles in this directory
+dotfiles=( $(ls -a) )
+
+# Skip the '.' and '..' directory names
+for file in ${dotfiles[@]:2}; do
+    # Skip the .git folder in dotfiles directory
+    if [ "$file" != ".git" ] && [ "$file" != "install.sh" ]
+    then 
+        # If file already exists in home, delete it.
+        if [ -e "~/$file" ]; then
+            rm -r "~/$file"
+            echo "File $file already existed. Deleting file"
+        fi
+
+        # Create the symlink to the dotfile in the home dir
+        ln -s "$cwd/$file" "~/$file"
+        echo "Creating symlink for $file"
+    fi
+done
