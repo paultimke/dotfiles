0b5c089c9cdce4d7f14bb65dfebf6464a0648612
Paul Timke <87957114+paultimke@users.noreply.github.com>
2022-12-29 14:28:32 -0600

    Add all files

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get the name of the current working directory (dotfiles folder)
+cwd=${PWD##*/}
+cwd=${cwd:-/}
+
+home_dir=~
+
+# Create an array with all the dotfiles in this directory
+dotfiles=( $(ls -a) )
+
+# Skip the '.' and '..' directory names
+for file in ${dotfiles[@]:2}; do
+    # Skip the .git folder in dotfiles directory
+    if [ "$file" != ".git" ] && [ "$file" != "install.sh" ]
+    then 
+        # If file already exists in home, delete it.
+        if [ -e "$home_dir/$file" ]; then
+            rm -r "$home_dir/$file"
+            echo "File $file already existed. Deleting file"
+        fi
+
+        # Create the symlink to the dotfile in the home dir
+        ln -s "$cwd/$file" ~ 
+        echo "Creating symlink for $file"
+    fi
+done
